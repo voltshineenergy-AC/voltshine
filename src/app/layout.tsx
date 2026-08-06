61b68a95f0b9905e40cbdc47a8413a5c0da6d72e
Nikhil Yadav <voltshineenergy@gmail.com>
2026-08-06 16:14:14 +0530

    Added Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,6 +99,15 @@ export default function RootLayout({
       gtag('config', 'G-EW70L5C17N');
     `}
   </Script>
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xy3e4qrp23");
+  `}
+</Script>
 
   <script
     type="application/ld+json"
